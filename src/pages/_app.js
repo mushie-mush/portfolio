14fe98623505aadc8074cf9b5e9aac0279b12d96
mush-mush-mush <shroom.mish12@gmail.com>
2023-02-05 16:18:00 +0800

    fix meta tags issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,42 @@ const raleway = Raleway({ subsets: ['latin'] })
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Made by Cello</title>
+        <meta
+          name="description"
+          content="Hi, my name is Marcello Sebastian, a javascript developer who focuses on modern front end web development."
+        />
+        <link rel="icon" type="image/x-icon" href="./images/favicon.ico"></link>
+
+        <meta property="og:url" content="https://madebycello.netlify.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Made by Cello" />
+        <meta
+          property="og:description"
+          content="Hi, my name is Marcello Sebastian, a javascript developer who focuses on modern front end web development."
+        />
+        <meta
+          property="og:image"
+          content="https://madebycello.netlify.app/og.png"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="madebycello.netlify.app" />
+        <meta
+          property="twitter:url"
+          content="https://madebycello.netlify.app/"
+        />
+        <meta name="twitter:title" content="Made by Cello" />
+        <meta
+          name="twitter:description"
+          content="Hi, my name is Marcello Sebastian, a javascript developer who focuses on modern front end web development."
+        />
+        <meta
+          name="twitter:image"
+          content="https://madebycello.netlify.app/og.png"
+        />
+      </Head>
       <style jsx global>{`
         html {
           font-family: ${raleway.style.fontFamily};
--- src/pages/index.js
+++ src/pages/index.js
@@ -26,42 +26,6 @@ export default function Home({ projects }) {
 
   return (
     <>
-      <Head>
-        <title>Made by Cello</title>
-        <meta
-          name="description"
-          content="Hi, my name is Marcello Sebastian, a javascript developer who focuses on modern front end web development."
-        />
-        <link rel="icon" type="image/x-icon" href="./images/favicon.ico"></link>
-
-        <meta property="og:url" content="https://madebycello.netlify.app/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Made by Cello" />
-        <meta
-          property="og:description"
-          content="Hi, my name is Marcello Sebastian, a javascript developer who focuses on modern front end web development."
-        />
-        <meta
-          property="og:image"
-          content="https://madebycello.netlify.app/og.png"
-        />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="madebycello.netlify.app" />
-        <meta
-          property="twitter:url"
-          content="https://madebycello.netlify.app/"
-        />
-        <meta name="twitter:title" content="Made by Cello" />
-        <meta
-          name="twitter:description"
-          content="Hi, my name is Marcello Sebastian, a javascript developer who focuses on modern front end web development."
-        />
-        <meta
-          name="twitter:image"
-          content="https://madebycello.netlify.app/og.png"
-        />
-      </Head>
       <Hero />
       <About />
       <Works projects={projects} />
