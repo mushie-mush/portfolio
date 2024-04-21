942570a42795fd89e0ca60024471022ad27d75bc
mushie-mush <marcello.sebastian09@gmail.com>
2024-04-21 11:52:30 +0800

    fix google analytics tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <GoogleAnalytics gaId="G-9MYVVH6KZW" />
+      <Head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-9MYVVH6KZW"></Script>
+        <Script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-9MYVVH6KZW');
+        </Script>
+      </Head>
+
       <body>
         <Main />
         <NextScript />
