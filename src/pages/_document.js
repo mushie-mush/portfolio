7596ea8096b522cb9ce4417086a88b9fc1f8f1ba
mushie-mush <marcello.sebastian09@gmail.com>
2024-04-21 11:36:43 +0800

    add google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <GoogleAnalytics gaId="G-9MYVVH6KZW" />
       <body>
         <Main />
         <NextScript />
